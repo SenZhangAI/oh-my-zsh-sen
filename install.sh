6edd169ed74ee607c1fae4d5b9a2745ee0fc3633
Sen Zhang <szhang.hust@gmail.com>
2015-12-05 11:15:37 +0800

    better install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
+
+# ********************************************************************
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+# ********************************************************************
+# check install
+CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
+if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
+  printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
+  exit
+fi
+unset CHECK_ZSH_INSTALLED
+
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+
+if [ ! -d "$ZSH" ]; then
+  printf "You may not install ${YELLOW}[Oh-My-Zsh]${NORMAL}.\n"
+  printf "You need to install it first.\n"
+  exit
+fi
+
+
+# ********************************************************************
+# install start
 echo 'install start...'
 
 if [ ! -d $ZSH/custom/plugins/zsh-syntax-highlighting ]; then
@@ -8,8 +53,15 @@ fi
 
 echo ' + install sen.zshenv.zsh...'
 cp $(PWD)/sen.zshenv.zsh $ZSH/custom/
+
 #echo ' + install sen.zshrc.zsh...'
 #cp $(PWD)/sen.zshrc.zsh $ZSH/custom/
+
+## install theme
 echo ' + install sen.zsh-theme...'
-cp $(PWD)/sen_cyg.zsh-theme $ZSH/themes/
+if [ ! -d $ZSH/custom/themes ]; then
+  mkdir $ZSH/custom/themes
+fi
+
+cp $(PWD)/sen_cyg.zsh-theme $ZSH/custom/themes
 echo 'done.'
--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,8 @@
 echo 'uninstall start...'
 echo ' - remove sen.zshenv.zsh...'
 rm $ZSH/custom/sen.zshenv.zsh
-echo ' - remove sen.zshrc.zsh...'
+#echo ' - remove sen.zshrc.zsh...'
 #rm $ZSH/custom/sen.zshrc.zsh
-#echo ' - remove sen.zsh-theme...'
-rm $ZSH/themes/sen_cyg.zsh-theme
+echo ' - remove sen.zsh-theme...'
+rm $ZSH/custom/themes/sen_cyg.zsh-theme
 echo 'done.'
