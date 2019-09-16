d081173a47796e7aab9da4b52df699838d1f7468
Sen Zhang <szhang.hust@gmail.com>
2019-09-16 14:01:08 +0800

    delete tput in install.sh, make it work well at kali OS

--- install.sh
+++ install.sh
@@ -1,32 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# ********************************************************************
-# Use colors, but only if connected to a terminal, and that terminal
-# supports them.
-if which tput >/dev/null 2>&1; then
-    ncolors=$(tput colors)
-fi
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-    RED="$(tput setaf 1)"
-    GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
-    NORMAL="$(tput sgr0)"
-else
-    RED=""
-    GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
-    NORMAL=""
-fi
+RED() {
+    printf "\033[31m$1\033[0m"
+}
 
 # ********************************************************************
 # check install
 CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
 if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
-    printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
+    printf "$(RED "Zsh is not installed!") Please install zsh first!\n"
     exit
 fi
 unset CHECK_ZSH_INSTALLED
